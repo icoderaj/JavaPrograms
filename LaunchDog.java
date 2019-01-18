29eb07b0f2ddc1dd2d5509b892cfec580d3d6b57
rp84 <45449580+rp84@users.noreply.github.com>
2019-01-18 15:03:17 +0530

    Update LaunchDog.java
    Added comments here

--- LaunchDog.java
+++ LaunchDog.java
@@ -1,12 +1,12 @@
 class Dog 
 {
-	//has properties
+	//A class has certain properties
 	String name;
 	String breed;
 	int cost;
 	String colour;
 
-	//does behaviours
+	//A class exhibit certain behaviours
 	void bark()
 	{
 		System.out.println("Dog is barking.");
@@ -24,19 +24,23 @@ class Dog
 
 }
 
+//Program will execute if main() is present inside Dog class but the main() will have no meaning as a behaviour of Dog class
+//Hence we have declared main() in another class => the file name should be same as the class name containing main() as the execution begins from main()
 class LaunchDog
 {
 	public static void main(String args[])
 	{
+		//new operator will make JVM create an object of the given class
 		Dog d1 = new Dog();
 		Dog d2 = new Dog();
-
+		
+		//many objects with unique names can call behaviours
 		d1.sleep();
 		d2.bark();
 		d1.eat();
 		d2.sleep();
 		
-		//Only 1 anonymous object
+		//An anonymous object
 		new Dog().eat();
 	}
 }
